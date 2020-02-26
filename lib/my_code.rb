07d65f08d459503998c1f9add524b67a5550c9fe
Asad Jafri <asadhjafri@gmail.com>
2020-02-26 05:46:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,3 +47,13 @@ def reduce_to_total(source_array, starting_point=0)
   end
   return reduced_total
 end
+
+def reduce_to_all_true(source_array)
+  all_true = []
+  i = 0
+  while i < source_array.length do
+    return true
+    i += 1
+  end
+  return all_true
+end
